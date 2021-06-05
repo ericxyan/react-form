0d7a72b12ea990d1a29024a6f27494cbb4ca4f3a
eric yan <eric.yan828@gmail.com>
2021-06-04 21:34:19 -0400

    model to view binding

--- src/component/MyForm.jsx
+++ src/component/MyForm.jsx
@@ -1,6 +1,24 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+
+const defaultData = {
+    firstName: "",
+    lastName: "",
+    gender: "",
+    birthday: undefined,
+    age: undefined,
+    country: undefined,
+    accept: false,
+    note: "",
+};
 
 export default function MyForm() {
+    const [data, setData] = useState(defaultData);
+
+    const onValueChange = (event) => {
+        setData({
+            [event.target.name]: event.target.value
+        });
+    };
 
     return (
         <div className="container">
@@ -9,39 +27,39 @@ export default function MyForm() {
             <form id="form">
                 <div className="formGroup">
                     <label htmlFor="firstName">First Name: </label>
-                    <input type="text" name="firstName" id="firstName" />
+                    <input type="text" name="firstName" id="firstName" value={data.firstName} onChange={onValueChange}/>
                 </div>
 
                 <div className="formGroup">
-                    <label>Last Name: <input type="text" /></label>
+                    <label>Last Name: <input type="text" value={data.lastName} onChange={onValueChange} /></label>
                 </div>
 
                 <div className="formGroup">
                     <span>Gender: </span>
                     <label>
-                        <input type="radio" value="male" name="gender" />
+                        <input type="radio" value="male" name="gender" checked={data.gender == 'male'} readOnly/>
                             Male
                         </label>
 
                     <label>
-                        <input type="radio" value="female" name="gender" />
+                        <input type="radio" value="female" name="gender" checked={data.gender == 'female'} readOnly/>
                         Female
                     </label>
                 </div>
 
                 <div className="formGroup">
                     <label>Birthday: 
-                        <input type="date" />
+                        <input type="date" value={data.birthday} onChange={onValueChange}/>
                     </label>
                 </div>
 
                 <div className="formGroup">
-                    <label>Age: <input type="number" min="0"/></label>
+                    <label>Age: <input type="number" min="0" value={data.age} onChange={onValueChange}/></label>
                 </div>
 
                 <div className="formGroup">
                     <label>Country: 
-                        <select >
+                        <select value={data.country} onChange={onValueChange}>
                             <option value="Canada">Canada</option>
                             <option value="US">US</option>
                         </select>
@@ -49,16 +67,18 @@ export default function MyForm() {
                 </div>
 
                 <div className="formGroup">
-                    <label><input type="checkbox" />Accept</label> n
+                    <label><input type="checkbox" defaultChecked={data.accept} />Accept</label> n
                 </div>
 
                 <div className="formGroup">
-                    <label>Note: <textarea /></label>
+                    <label>Note: <textarea value={data.note} onChange={onValueChange}/></label>
                 </div>
 
                 <button type="reset">Reset</button>
                 <button type="submit">Submit</button>
             </form>
+
+            <pre>{JSON.stringify(data, null, 2)}</pre>
 
         </div>
     );
